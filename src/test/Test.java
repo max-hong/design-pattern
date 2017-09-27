7b43dd7681c89a6649e22a43936ae2c0f2be4d53
max-hong <289079931@qq.com>
2017-09-27 13:42:35 +0800

    tijiao

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,19 @@
 package test;
 
+import java.util.Arrays;
+
 public class Test implements TestInterface, TestInterface2 {
 
 	public static void main(String[] args) {
-		Test test = new Test();
-		test.run1();
-		test.run2();
+		/*
+		 * Test test = new Test(); test.run1(); test.run2();
+		 */
+
+		int[] scores = { 5,1,6,2 };
+		Arrays.sort(scores);
+
+		for (int i : scores) {
+			System.out.println(i);
+		}
 	}
 }
